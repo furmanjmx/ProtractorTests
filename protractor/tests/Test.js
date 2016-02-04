839a6ff9db604b2924d32c7d50396d79b96364a3
napster <evgeniy.furmann@gmail.com>
2016-02-04 17:19:20 +0200

    card 2 card test

--- protractor/tests/Test.js
+++ protractor/tests/Test.js
@@ -1,13 +1,91 @@
+describe('p2y', function () {
 
-describe('p2y. logo', function () {
+    var person = [
+        {
+            "number": 5591587543706253,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            }
+        },
+        {
+            "number": 4621236605015438,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            }
+        },
+        {
+            "number": 5513877294352240,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            },
+            "3ds_code": 123456
+        },
+        {
+            "number": 5473959513413611,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            },
+            "3ds_code": 123456
+        },
+        {
+            "number": 5232747764071184,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            }
+        },
+        {
+            "number": 4556051556352501,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            }
+        },
+        {
+            "number": 4485713355355987,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            },
+            "3ds_code": 123456
+        },
+        {
+            "number": 4716894147386794,
+            "cvv": 160,
+            "expired": {
+                "month": "01",
+                "year": "20"
+            },
+            "3ds_code": 123456
+        }
+    ];
 
-    it ('should exist', function () {
+    it('Card 2 card test', function () {
+
         browser.get('http://p2y.com.ua');
 
-        var logo = element(by.css('.logo')).element(by.tagName('img'));
+        var form = element(by.css('.form'));
+        
+        element(by.model('model.cardNumber')).sendKeys(person[0].number);
+        element(by.model('model.expMonth')).sendKeys(person[0].expired.month);
+        element(by.model('model.expYear')).sendKeys(person[0].expired.year);
+        element(by.model('model.cvv')).sendKeys(person[0].cvv);
+        element(by.model('model.phoneNumber')).sendKeys('931254212');
+        element(by.model('model.receiver.cardNumber')).sendKeys(person[0].number);
+        element(by.model('model.amount')).sendKeys('1');
 
-        expect(logo).toBeDefined();
-        expect(logo.getAttribute('src')).toEqual('https://p2y.com.ua/images/logo.png');
+        form.submit();
     });
 
 });
