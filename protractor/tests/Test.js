ef6fa1bb2204150fca59c8e3b7c58d0801acba05
napster <evgeniy.furmann@gmail.com>
2016-02-04 17:31:24 +0200

    add fixtures file

--- protractor/tests/Test.js
+++ protractor/tests/Test.js
@@ -1,88 +1,21 @@
+var fixtures = require('./fixtures');
+
 describe('p2y', function () {
 
-    var person = [
-        {
-            "number": 5591587543706253,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            }
-        },
-        {
-            "number": 4621236605015438,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            }
-        },
-        {
-            "number": 5513877294352240,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            },
-            "3ds_code": 123456
-        },
-        {
-            "number": 5473959513413611,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            },
-            "3ds_code": 123456
-        },
-        {
-            "number": 5232747764071184,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            }
-        },
-        {
-            "number": 4556051556352501,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            }
-        },
-        {
-            "number": 4485713355355987,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            },
-            "3ds_code": 123456
-        },
-        {
-            "number": 4716894147386794,
-            "cvv": 160,
-            "expired": {
-                "month": "01",
-                "year": "20"
-            },
-            "3ds_code": 123456
-        }
-    ];
 
     it('Card 2 card test', function () {
 
         browser.get('http://p2y.com.ua');
 
+        var card = fixtures.cards[0];
         var form = element(by.css('.form'));
-        
-        element(by.model('model.cardNumber')).sendKeys(person[0].number);
-        element(by.model('model.expMonth')).sendKeys(person[0].expired.month);
-        element(by.model('model.expYear')).sendKeys(person[0].expired.year);
-        element(by.model('model.cvv')).sendKeys(person[0].cvv);
+
+        element(by.model('model.cardNumber')).sendKeys(card.number);
+        element(by.model('model.expMonth')).sendKeys(card.expired.month);
+        element(by.model('model.expYear')).sendKeys(card.expired.year);
+        element(by.model('model.cvv')).sendKeys(card.cvv);
         element(by.model('model.phoneNumber')).sendKeys('931254212');
-        element(by.model('model.receiver.cardNumber')).sendKeys(person[0].number);
+        element(by.model('model.receiver.cardNumber')).sendKeys(card.number);
         element(by.model('model.amount')).sendKeys('1');
 
         form.submit();
---
+++ protractor/tests/fixtures.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports.cards = [
+    {
+        "number": 5591587543706253,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        }
+    },
+    {
+        "number": 4621236605015438,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        }
+    },
+    {
+        "number": 5513877294352240,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        },
+        "3ds_code": 123456
+    },
+    {
+        "number": 5473959513413611,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        },
+        "3ds_code": 123456
+    },
+    {
+        "number": 5232747764071184,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        }
+    },
+    {
+        "number": 4556051556352501,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        }
+    },
+    {
+        "number": 4485713355355987,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        },
+        "3ds_code": 123456
+    },
+    {
+        "number": 4716894147386794,
+        "cvv": 160,
+        "expired": {
+            "month": "01",
+            "year": "20"
+        },
+        "3ds_code": 123456
+    }
+];
