c9053be740ef7f967274ecedfca6ee04c0a81bf9
napster <evgeniy.furmann@gmail.com>
2016-02-05 13:05:33 +0200

    change url on DEV

--- protractor/tests/Cards.js
+++ protractor/tests/Cards.js
@@ -1,11 +1,8 @@
 var fixtures = require('./fixtures');
 
 describe('p2y', function () {
-
     it('Card 2 card test', function () {
-
-        browser.get('http://p2y.com.ua');
-
+        browser.get('http://nebo15.github.io/p2y.web');
         var card = fixtures.cards[0];
         var form = element(by.css('.form'));
         // fill form
@@ -16,8 +13,6 @@ describe('p2y', function () {
         element(by.model('model.phoneNumber')).sendKeys('931254212');
         element(by.model('model.receiver.cardNumber')).sendKeys(card.number);
         element(by.model('model.amount')).sendKeys('1');
-
         form.submit();
     });
-
 });
